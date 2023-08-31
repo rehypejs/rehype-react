c4577ec62863961d41fd15452715ea642bd063bd
Titus Wormer <tituswormer@gmail.com>
2023-08-31 16:24:13 +0200

    Refactor to use `node:test`

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import React from 'react'
 import server from 'react-dom/server'
 import {unified} from 'unified'
@@ -9,245 +10,289 @@ import rehypeReact from './index.js'
 const options = {createElement: React.createElement}
 const processor = unified().use(rehypeReact, options)
 
-test('React ' + React.version, (t) => {
-  t.throws(
-    () => {
+test('React ' + React.version, async function (t) {
+  await t.test('should fail without `createElement`', async function () {
+    assert.throws(function () {
       // @ts-expect-error: options missing.
       unified()
         .use(rehypeReact)
         .stringify(u('root', [h('p')]))
-    },
-    /^TypeError: createElement is not a function$/,
-    'should fail without `createElement`'
-  )
+    }, /^TypeError: createElement is not a function$/)
+  })
 
-  t.deepEqual(
-    processor.stringify(u('root', [h('p')])),
-    React.createElement('div', {}, [
-      React.createElement('p', {key: 'h-1'}, undefined)
-    ]),
-    'should transform a root'
-  )
-
-  t.deepEqual(
-    // To do: this should error, because it’s not a root.
-    processor.stringify(h('p')),
-    React.createElement('p', {key: 'h-1'}, undefined),
-    'should transform an element'
-  )
-
-  t.deepEqual(
-    processor.stringify(u('root', [h('h1.main-heading', {dataFoo: 'bar'})])),
-    React.createElement('div', {}, [
-      React.createElement(
-        'h1',
-        {className: 'main-heading', 'data-foo': 'bar', key: 'h-1'},
-        undefined
-      )
-    ]),
-    'should transform an element with properties'
-  )
-
-  t.deepEqual(
-    processor.stringify(u('root', [h('p', 'baz')])),
-    React.createElement('div', {}, [
-      React.createElement('p', {key: 'h-1'}, ['baz'])
-    ]),
-    'should transform an element with a text node'
-  )
-
-  t.deepEqual(
-    processor.stringify(u('root', [h('p', h('strong', 'qux'))])),
-    React.createElement('div', {}, [
-      React.createElement('p', {key: 'h-1'}, [
-        React.createElement('strong', {key: 'h-2'}, ['qux'])
-      ])
-    ]),
-    'should transform an element with a child element'
-  )
-
-  t.deepEqual(
-    processor.stringify(
-      u('root', [h('p', [h('em', 'qux'), ' foo ', h('i', 'bar')])])
-    ),
-    React.createElement('div', {}, [
-      React.createElement('p', {key: 'h-1'}, [
-        React.createElement('em', {key: 'h-2'}, ['qux']),
-        ' foo ',
-        React.createElement('i', {key: 'h-3'}, ['bar'])
-      ])
-    ]),
-    'should transform an element with mixed contents'
-  )
-
-  t.deepEqual(
-    processor.stringify(u('root', [h('p')])),
-    React.createElement('div', {}, [
-      React.createElement('p', {key: 'h-1'}, undefined)
-    ]),
-    'should transform `root` to a `div` by default'
-  )
-
-  t.deepEqual(
-    unified()
-      .use(rehypeReact, {
-        createElement: React.createElement,
-        Fragment: React.Fragment
-      })
-      .stringify(u('root', [h('h1'), h('p')])),
-    React.createElement(React.Fragment, {}, [
-      React.createElement('h1', {key: 'h-2'}, undefined),
-      React.createElement('p', {key: 'h-3'}, undefined)
-    ]),
-    'should transform `root` to a `Fragment` if given'
-  )
-
-  t.deepEqual(
-    processor.stringify(u('root', [u('doctype', {name: 'html'})])),
-    React.createElement('div', {}, undefined),
-    'should skip `doctype`s'
-  )
-
-  t.deepEqual(
-    processor.stringify(
-      u('root', [
-        h('section', h('h1.main-heading', {dataFoo: 'bar'}, h('span', 'baz')))
-      ])
-    ),
-    React.createElement('div', {}, [
-      React.createElement('section', {key: 'h-1'}, [
-        React.createElement(
-          'h1',
-          {
-            key: 'h-2',
-            className: 'main-heading',
-            'data-foo': 'bar'
-          },
-          [React.createElement('span', {key: 'h-3'}, ['baz'])]
-        )
-      ])
-    ]),
-    'should transform trees'
-  )
-
-  t.deepEqual(
-    server.renderToStaticMarkup(
-      // @ts-expect-error: to do: figure out.
-      unified()
-        .use(rehypeReact, {
-          createElement: React.createElement,
-          components: {
-            /** @param {object} props */
-            h1: (props) => React.createElement('h2', props)
-          }
-        })
-        .stringify(u('root', [h('h1')]))
-    ),
-    server.renderToStaticMarkup(
+  await t.test('should transform a root', async function () {
+    assert.deepEqual(
+      processor.stringify(u('root', [h('p')])),
       React.createElement('div', {}, [
-        React.createElement('h2', {key: 'h-1'}, undefined)
+        React.createElement('p', {key: 'h-1'}, undefined)
       ])
-    ),
-    'should support components'
-  )
+    )
+  })
 
-  t.deepEqual(
-    processor.stringify(
-      u('root', [h('table', {}, [h('thead', h('th', {align: 'right'}))])])
-    ),
-    React.createElement('div', {}, [
-      React.createElement('table', {key: 'h-1'}, [
-        React.createElement('thead', {key: 'h-2'}, [
+  await t.test('should transform an element', async function () {
+    assert.deepEqual(
+      // To do: this should error, because it’s not a root.
+      processor.stringify(h('p')),
+      React.createElement('p', {key: 'h-1'}, undefined)
+    )
+  })
+
+  await t.test(
+    'should transform an element with properties',
+    async function () {
+      assert.deepEqual(
+        processor.stringify(
+          u('root', [h('h1.main-heading', {dataFoo: 'bar'})])
+        ),
+        React.createElement('div', {}, [
           React.createElement(
-            'th',
-            {style: {textAlign: 'right'}, key: 'h-3'},
+            'h1',
+            {className: 'main-heading', 'data-foo': 'bar', key: 'h-1'},
             undefined
           )
         ])
-      ])
-    ]),
-    'should transform an element with align property'
+      )
+    }
   )
 
-  t.deepEqual(
-    processor.stringify(
-      u('root', [
-        h('table', {}, [
-          '\n  ',
-          h('tbody', {}, [
-            '\n  ',
-            h('tr', {}, ['\n  ', h('th', {}, ['\n  ']), h('td', {}, ['\n  '])])
+  await t.test(
+    'should transform an element with a text node',
+    async function () {
+      assert.deepEqual(
+        processor.stringify(u('root', [h('p', 'baz')])),
+        React.createElement('div', {}, [
+          React.createElement('p', {key: 'h-1'}, ['baz'])
+        ])
+      )
+    }
+  )
+
+  await t.test(
+    'should transform an element with a child element',
+    async function () {
+      assert.deepEqual(
+        processor.stringify(u('root', [h('p', h('strong', 'qux'))])),
+        React.createElement('div', {}, [
+          React.createElement('p', {key: 'h-1'}, [
+            React.createElement('strong', {key: 'h-2'}, ['qux'])
           ])
         ])
-      ])
-    ),
-    React.createElement('div', {}, [
-      React.createElement('table', {key: 'h-1'}, [
-        React.createElement('tbody', {key: 'h-2'}, [
-          React.createElement('tr', {key: 'h-3'}, [
-            React.createElement('th', {key: 'h-4'}, ['\n  ']),
-            React.createElement('td', {key: 'h-5'}, ['\n  '])
+      )
+    }
+  )
+
+  await t.test(
+    'should transform an element with mixed contents',
+    async function () {
+      assert.deepEqual(
+        processor.stringify(
+          u('root', [h('p', [h('em', 'qux'), ' foo ', h('i', 'bar')])])
+        ),
+        React.createElement('div', {}, [
+          React.createElement('p', {key: 'h-1'}, [
+            React.createElement('em', {key: 'h-2'}, ['qux']),
+            ' foo ',
+            React.createElement('i', {key: 'h-3'}, ['bar'])
           ])
         ])
-      ])
-    ]),
-    'should transform a table with whitespace'
+      )
+    }
   )
 
-  const headingNode = h('h1')
-  /** @param {object} props */
-  const Heading1 = function (props) {
-    return React.createElement('h1', props)
-  }
-
-  t.deepEqual(
-    unified()
-      .use(rehypeReact, {
-        createElement: React.createElement,
-        passNode: true,
-        components: {h1: Heading1}
-      })
-      .stringify(u('root', [headingNode, h('p')])),
-    React.createElement('div', {}, [
-      // @ts-expect-error: yeah it’s not okay per react types, but it works fine.
-      React.createElement(Heading1, {key: 'h-2', node: headingNode}, undefined),
-      React.createElement('p', {key: 'h-3'}, undefined)
-    ]),
-    'should expose node from node prop'
+  await t.test(
+    'should transform `root` to a `div` by default',
+    async function () {
+      assert.deepEqual(
+        processor.stringify(u('root', [h('p')])),
+        React.createElement('div', {}, [
+          React.createElement('p', {key: 'h-1'}, undefined)
+        ])
+      )
+    }
   )
 
-  t.deepEqual(
-    unified()
-      .use(rehypeReact, {
-        createElement: React.createElement,
-        fixTableCellAlign: false
-      })
-      .stringify(
+  await t.test(
+    'should transform `root` to a `Fragment` if given',
+    async function () {
+      assert.deepEqual(
+        unified()
+          .use(rehypeReact, {
+            createElement: React.createElement,
+            Fragment: React.Fragment
+          })
+          .stringify(u('root', [h('h1'), h('p')])),
+        React.createElement(React.Fragment, {}, [
+          React.createElement('h1', {key: 'h-2'}, undefined),
+          React.createElement('p', {key: 'h-3'}, undefined)
+        ])
+      )
+    }
+  )
+
+  await t.test('should skip `doctype`s', async function () {
+    assert.deepEqual(
+      processor.stringify(u('root', [u('doctype', {name: 'html'})])),
+      React.createElement('div', {}, undefined)
+    )
+  })
+
+  await t.test('should transform trees', async function () {
+    assert.deepEqual(
+      processor.stringify(
         u('root', [
-          h('table', {align: 'top'}, [
+          h('section', h('h1.main-heading', {dataFoo: 'bar'}, h('span', 'baz')))
+        ])
+      ),
+      React.createElement('div', {}, [
+        React.createElement('section', {key: 'h-1'}, [
+          React.createElement(
+            'h1',
+            {
+              key: 'h-2',
+              className: 'main-heading',
+              'data-foo': 'bar'
+            },
+            [React.createElement('span', {key: 'h-3'}, ['baz'])]
+          )
+        ])
+      ])
+    )
+  })
+
+  await t.test('should support components', async function () {
+    assert.deepEqual(
+      server.renderToStaticMarkup(
+        // @ts-expect-error: to do: figure out.
+        unified()
+          .use(rehypeReact, {
+            createElement: React.createElement,
+            components: {
+              /** @param {object} props */
+              h1(props) {
+                return React.createElement('h2', props)
+              }
+            }
+          })
+          .stringify(u('root', [h('h1')]))
+      ),
+      server.renderToStaticMarkup(
+        React.createElement('div', {}, [
+          React.createElement('h2', {key: 'h-1'}, undefined)
+        ])
+      )
+    )
+  })
+
+  await t.test(
+    'should transform an element with align property',
+    async function () {
+      assert.deepEqual(
+        processor.stringify(
+          u('root', [h('table', {}, [h('thead', h('th', {align: 'right'}))])])
+        ),
+        React.createElement('div', {}, [
+          React.createElement('table', {key: 'h-1'}, [
+            React.createElement('thead', {key: 'h-2'}, [
+              React.createElement(
+                'th',
+                {style: {textAlign: 'right'}, key: 'h-3'},
+                undefined
+              )
+            ])
+          ])
+        ])
+      )
+    }
+  )
+
+  await t.test('should transform a table with whitespace', async function () {
+    assert.deepEqual(
+      processor.stringify(
+        u('root', [
+          h('table', {}, [
             '\n  ',
             h('tbody', {}, [
               '\n  ',
               h('tr', {}, [
+                '\n  ',
                 h('th', {}, ['\n  ']),
-                h('td', {align: 'center'}, ['\n  '])
+                h('td', {}, ['\n  '])
               ])
             ])
           ])
         ])
       ),
-    React.createElement('div', {}, [
-      React.createElement('table', {key: 'h-1', align: 'top'}, [
-        React.createElement('tbody', {key: 'h-2'}, [
-          React.createElement('tr', {key: 'h-3'}, [
-            React.createElement('th', {key: 'h-4'}, ['\n  ']),
-            React.createElement('td', {key: 'h-5', align: 'center'}, ['\n  '])
+      React.createElement('div', {}, [
+        React.createElement('table', {key: 'h-1'}, [
+          React.createElement('tbody', {key: 'h-2'}, [
+            React.createElement('tr', {key: 'h-3'}, [
+              React.createElement('th', {key: 'h-4'}, ['\n  ']),
+              React.createElement('td', {key: 'h-5'}, ['\n  '])
+            ])
           ])
         ])
       ])
-    ]),
-    'should respect mapDepricatedTableCellAttrsToInlineCss option'
-  )
+    )
+  })
 
-  t.end()
+  await t.test('should expose node from node prop', async function () {
+    const headingNode = h('h1')
+    /** @param {object} props */
+    const Heading1 = function (props) {
+      return React.createElement('h1', props)
+    }
+
+    assert.deepEqual(
+      unified()
+        .use(rehypeReact, {
+          createElement: React.createElement,
+          passNode: true,
+          components: {h1: Heading1}
+        })
+        .stringify(u('root', [headingNode, h('p')])),
+      React.createElement('div', {}, [
+        React.createElement(
+          Heading1,
+          // @ts-expect-error: yeah it’s not okay per react types, but it works fine.
+          {key: 'h-2', node: headingNode},
+          undefined
+        ),
+        React.createElement('p', {key: 'h-3'}, undefined)
+      ])
+    )
+  })
+
+  await t.test('should respect `fixTableCellAlign` option', async function () {
+    assert.deepEqual(
+      unified()
+        .use(rehypeReact, {
+          createElement: React.createElement,
+          fixTableCellAlign: false
+        })
+        .stringify(
+          u('root', [
+            h('table', {align: 'top'}, [
+              '\n  ',
+              h('tbody', {}, [
+                '\n  ',
+                h('tr', {}, [
+                  h('th', {}, ['\n  ']),
+                  h('td', {align: 'center'}, ['\n  '])
+                ])
+              ])
+            ])
+          ])
+        ),
+      React.createElement('div', {}, [
+        React.createElement('table', {key: 'h-1', align: 'top'}, [
+          React.createElement('tbody', {key: 'h-2'}, [
+            React.createElement('tr', {key: 'h-3'}, [
+              React.createElement('th', {key: 'h-4'}, ['\n  ']),
+              React.createElement('td', {key: 'h-5', align: 'center'}, ['\n  '])
+            ])
+          ])
+        ])
+      ])
+    )
+  })
 })
